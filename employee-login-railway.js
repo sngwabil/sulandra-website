cc0efc0e68e7ec518953e3cd22f369206721a033
sngwabil <Sulpitius.gwabil@gmail.com>
2026-08-04 20:09:48 -0400

    Point employee login to active Railway API

--- employee-login-railway.js
+++ employee-login-railway.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const API_BASE = "https://sulandra-website-production.up.railway.app";
+  const API_BASE = "https://sulandra-website-production-5fc4.up.railway.app";
   const TOKEN_KEY = "sulandra:employee:access-token";
   const SESSION_KEY = "sulandra:employee:session";
   const message = document.getElementById("msg");
