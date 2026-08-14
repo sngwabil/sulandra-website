ff1409c30021c7d33e254831c8b805499c4ba422
sngwabil <Sulpitius.gwabil@gmail.com>
2026-08-13 21:38:18 -0400

    Load SPIRE theme selector extension

---
+++ assets/spire-theme-selector-loader.js
@@ -0,0 +1,6 @@
+(() => {
+  const script = document.createElement('script');
+  script.src = '/assets/spire-theme-selector-extension.js?v=20260813-theme22';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
