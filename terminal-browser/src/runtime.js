9219f1bddaa60b162b2fdab3bd734aeab06d1d59
sngwabil <Sulpitius.gwabil@gmail.com>
2026-08-30 18:09:44 -0400

    Load ligatures after xterm opens in browser

--- terminal-browser/src/runtime.js
+++ terminal-browser/src/runtime.js
@@ -1,4 +1,4 @@
-import { Terminal } from '@xterm/xterm';
+import { Terminal as XtermTerminal } from '@xterm/xterm';
 import { WebglAddon } from '@xterm/addon-webgl';
 import { CanvasAddon } from '@xterm/addon-canvas';
 import { FitAddon } from '@xterm/addon-fit';
@@ -8,6 +8,21 @@ import { ImageAddon } from '@xterm/addon-image';
 import { Unicode11Addon } from '@xterm/addon-unicode11';
 import { SerializeAddon } from '@xterm/addon-serialize';
 import { LigaturesAddon } from '@xterm/addon-ligatures';
+
+class Terminal extends XtermTerminal {
+  open(parent) {
+    super.open(parent);
+    if (!this.__sulandraLigaturesAddon) {
+      try {
+        const ligatures = new LigaturesAddon();
+        this.loadAddon(ligatures);
+        this.__sulandraLigaturesAddon = ligatures;
+      } catch (error) {
+        console.warn('[Sulandra Terminal] ligatures unavailable; continuing without them', error);
+      }
+    }
+  }
+}
 
 export {
   Terminal,
